be05a862aceb823d50026b557cb01a7b72364c9a
Shravani PL <pl.shravani@gmail.com>
2018-06-04 19:02:29 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -5,7 +5,7 @@
 def position_taken?(board,index)
   if(board[index] == nil || board[index] == " " || board[index] == "")
     "false"
-  else
+  elsif(board[index] == 'X' || board[index] == 'O')
     "true"
   end
 end
